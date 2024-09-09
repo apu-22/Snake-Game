234c101861159d14b1c8727f565f662eb9a39afc
Apu Rayhan <mdapurayhan1729@gmail.com>
2024-09-09 12:10:47 +0600

    Add snake game cover photo

--- snake.cpp
+++ snake.cpp
@@ -1,77 +1,108 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 
 using namespace std;
 
-bool CreateWindowAndRenderer(SDL_Window *&window, SDL_Renderer *&renderer, const char *title, int width, int height)
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
+
+bool initializeSDL(SDL_Window *&window, SDL_Renderer *&renderer)
 {
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        cout << "Failed to initialize SDL: " << SDL_GetError() << endl;
+        cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << endl;
         return false;
     }
 
-    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
-    if (!window)
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
     {
-        cout << "Failed to create window: " << SDL_GetError() << endl;
-        SDL_Quit();
+        cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << endl;
+        return false;
+    }
+
+    window = SDL_CreateWindow("SDL Full-Window Image", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr)
+    {
+        cout << "Window could not be created! SDL Error: " << SDL_GetError() << endl;
         return false;
     }
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer)
+    if (renderer == nullptr)
     {
-        cout << "Failed to create renderer: " << SDL_GetError() << endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+        cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << endl;
         return false;
     }
 
     return true;
 }
 
-void GameLoop(SDL_Window *window, SDL_Renderer *renderer)
+bool loadAndRenderImage(SDL_Renderer *renderer, const char *filePath)
 {
-    bool running = true;
-    SDL_Event event;
-
-    while (running)
+    SDL_Texture *texture = IMG_LoadTexture(renderer, filePath);
+    if (texture == nullptr)
     {
-        while (SDL_PollEvent(&event))
+        std::cout << "Failed to load texture! SDL_image Error: " << IMG_GetError() << std::endl;
+        return false;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 205, 20, 205, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_DestroyTexture(texture);
+    return true;
+}
+
+void GameLoop(SDL_Renderer *renderer)
+{
+    SDL_Event e;
+    bool quit = false;
+
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e) != 0)
         {
-            if (event.type == SDL_QUIT)
+            if (e.type == SDL_QUIT)
             {
-                running = false;
+                quit = true;
             }
         }
-
-        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
     }
 }
 
-void Cleanup(SDL_Window *window, SDL_Renderer *renderer)
+void cleanUp(SDL_Window *window, SDL_Renderer *renderer)
 {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    IMG_Quit();
     SDL_Quit();
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char *args[])
 {
-    SDL_Window *window = nullptr;
-    SDL_Renderer *renderer = nullptr;
-
-    if (!CreateWindowAndRenderer(window, renderer, "SDL2 Modular Window", 800, 600))
+    if (!initializeSDL(window, renderer))
     {
-        return 1;
+        cleanUp(window, renderer);
+        return -1;
     }
 
-    GameLoop(window, renderer);
+    if (!loadAndRenderImage(renderer, "image/cover_photo.png"))
+    {
+        cleanUp(window, renderer);
+        return -1;
+    }
 
-    Cleanup(window, renderer);
+    GameLoop(renderer);
+
+    cleanUp(window, renderer);
 
     return 0;
 }
